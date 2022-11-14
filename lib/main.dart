5ceb5f05fc31800a311ecbdfe0fba0b3f76bd192
Muhammad Ijaz <ijazsharif34@gmail.com>
2022-11-14 10:43:02 +0000

    imports organized

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:hive/hive.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:new_wall/ui/theme/theme.dart';
 import 'package:new_wall/ui/widgets/constants.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'firebase_options.dart';
+
 import 'ui/screens/main_screen/main_screen.dart';
 
 Future<void> main() async {
